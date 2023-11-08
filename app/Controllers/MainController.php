98ae231af08be7da6e883cdfba90cbc0de7da5ca
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-08 20:16:44 +0800

    backend

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\RestFul\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\MainModel;
+
+class MainController extends ResourceController
+{
+    public function index()
+    {
+        // Your index logic here
+    }
+
+    public function getData()
+    {
+        $main = new MainModel();
+        $data = $main->findAll();
+        return $this->respond($data);
+    }
+}
