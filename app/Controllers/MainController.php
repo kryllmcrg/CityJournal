27b4b0064ae7d345cc213a678cc821ee26ccda32
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-11 13:05:02 +0800

    Update MainController.php

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,38 +1,26 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\Restful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\LogModel;
-use App\Models\RegModel;
-
 
 class MainController extends ResourceController
 {
-    public function login()
+    public function index()
     {
-        $request = $this->request;
-
-        // Validate input
-        $validationRules = [
-            'username' => 'required|valid_username',
-            'password' => 'required|min_length[6]',
-        ];
-
-        if (!$this->validate($validationRules)) {
-            return $this->fail($this->validator->getErrors(), 400);
-        }
-
-        $mainModel = new LogModel();
-        $user = $mainModel->where('username', $request->getPost('username'))->first();
-
-        if (!$user || !password_verify($request->getPost('password'), $user['password'])) {
-            return $this->fail('Invalid username or password', 401);
-        }
-
-        return $this->respond(['message' => 'Login successful'], 200);
+        //
     }
+
+    public function getData()
+    {
+        $Log = new LogModel();
+        $data = $Log->findAll();
+        return $this->respond($data, 200);
+    }
+
     public function register()
     {
         $request = $this->request;
@@ -48,13 +36,37 @@ class MainController extends ResourceController
             return $this->fail($this->validator->getErrors(), 400);
         }
 
-        $mainModel = new RegModel();
-        $mainModel->save([
+        $LogModel = new LogModel();
+        $LogModel->save([
             'username' => $request->getVar('username'),
             'email' => $request->getVar('email'),
             'password' => password_hash($request->getVar('password'), PASSWORD_BCRYPT),            
         ]);
 
         return $this->respond(['message' => 'Registration successful'], 201);
+    }
+
+    public function login()
+    {
+        $request = $this->request;
+
+        // Validate input
+        $validationRules = [
+            'username' => 'required|valid_username',
+            'password' => 'required|min_length[6]',
+        ];
+
+        if (!$this->validate($validationRules)) {
+            return $this->fail($this->validator->getErrors(), 400);
+        }
+
+        $LogModel = new LogModel();
+        $user = $LogModel->where('username', $request->getPost('username'))->first();
+
+        if (!$user || !password_verify($request->getPost('password'), $user['password'])) {
+            return $this->fail('Invalid password', 401);
+        }
+
+        return $this->respond(['message' => 'Login successful'], 200);
     }
 }
