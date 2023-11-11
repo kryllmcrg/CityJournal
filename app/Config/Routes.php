1803d188a141b527831ca309198f87730ee39b55
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-11 11:30:06 +0800

    backend

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/getData', 'MainController::getData');
 $routes->post('/login', 'MainController::login');
+$routes->post('/register', 'MainController::register');
--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,33 +1,59 @@
 <?php
-
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\RestFul\ResourceController;
+use CodeIgniter\Restful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\MainModel;
 
+
 class MainController extends ResourceController
 {
-    public function index()
-    {
-        //
-    }
     public function login()
     {
-        $json = $this->request->getJSON();
-        $data =[
-            'username' => $json->username,
-            'password' => $json->password,
+        $request = $this->request;
+
+        // Validate input
+        $validationRules = [
+            'username' => 'required|valid_username',
+            'password' => 'required|min_length[6]',
         ];
-        $main = new MainModel();
-        $r =$main->login($data);
-        return $this->respond($r, 200);
+
+        if (!$this->validate($validationRules)) {
+            return $this->fail($this->validator->getErrors(), 400);
+        }
+
+        $mainModel = new MainModel();
+        $user = $mainModel->where('username', $request->getPost('username'))->first();
+
+        if (!$user || !password_verify($request->getPost('password'), $user['password'])) {
+            return $this->fail('Invalid username or password', 401);
+        }
+
+        return $this->respond(['message' => 'Login successful'], 200);
     }
-    public function getData()
+    public function register()
     {
-        $main = new MainModel();
-        $data = $main->findAll();
-        return $this->respond($data);
+        $request = $this->request;
+
+        // Validate input
+        $validationRules = [
+            'username' => 'required|min_length[3]',
+            'email' => 'required|valid_email|is_unique[users.email]',
+            'password' => 'required|min_length[6]',
+        ];
+
+        if (!$this->validate($validationRules)) {
+            return $this->fail($this->validator->getErrors(), 400);
+        }
+
+        $mainModel = new MainModel();
+        $mainModel->save([
+            'username' => $request->getVar('username'),
+            'email' => $request->getVar('email'),
+            'password' => password_hash($request->getVar('password'), PASSWORD_BCRYPT),            
+        ]);
+
+        return $this->respond(['message' => 'Registration successful'], 201);
     }
 }
