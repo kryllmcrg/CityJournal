5b2f1a5a0396ce57135bac8bdace97c38b6887e2
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-14 12:24:12 +0800

    frontend

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="container">
-    <v-row>
+    <v-row align="center">
       <v-col cols="12">
         <v-app-bar app color="#673ab7" dark>
           <v-toolbar-title>
@@ -12,21 +12,22 @@
 
     <!-- Main Content -->
     <v-row justify="center">
-      <v-col cols="12" sm="10" md="10" lg="5">
-        <v-card>
+      <v-col cols="12" sm="10" md="8" lg="6">
+        <v-card elevation="12">
           <v-card-title class="headline">
             Login
           </v-card-title>
 
           <v-card-text>
             <v-form ref="loginForm" @submit.prevent="login">
-              <v-text-field v-model="username" label="Username" required></v-text-field>
+              <v-text-field v-model="username" label="Username" outlined required></v-text-field>
               <v-text-field
                 v-model="password"
                 label="Password"
                 :type="passwordVisible ? 'text' : 'password'"
                 :append-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
                 @click:append="togglePasswordVisibility"
+                outlined
                 required
               ></v-text-field>
               <v-btn type="submit" color="#673ab7" class="mr-4">
@@ -34,7 +35,7 @@
               </v-btn>
             </v-form>
 
-            <v-row justify="center">
+            <v-row justify="center" class="mt-4">
               <router-link to="/register" class="link">
                 Register
               </router-link>
@@ -112,12 +113,30 @@ export default {
 .separator {
   margin: 0 10px;
 }
-.container{
+
+.container {
   background-image: url('@/assets/bglogin.jpg');
   background-size: cover;
   background-position: center;
+<<<<<<< Updated upstream
   height: 100vh; /* Set the height based on your design */
   align-items: center;
   justify-content: center;
+=======
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.v-card {
+  background-color: #ffffff; /* Card background color */
+  border-radius: 10px;
+  padding: 20px;
+}
+
+.v-btn {
+  border-radius: 5px;
+>>>>>>> Stashed changes
 }
 </style>
