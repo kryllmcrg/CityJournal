547b7504735f4546e27ec5c027d571ca4216d92c
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-14 12:57:35 +0800

    Update register.vue

--- src/components/register.vue
+++ src/components/register.vue
@@ -2,19 +2,41 @@
   <div class="d-flex align-center justify-center" style="height: 100vh">
     <v-sheet width="400" class="mx-auto">
       <v-form @submit.prevent="register">
-        <v-text-field variant="outlined" v-model="username" label="Username"></v-text-field>
+        <v-text-field
+          variant="outlined"
+          v-model="username"
+          label="Username"
+        ></v-text-field>
 
         <v-text-field variant="outlined" v-model="email" label="Email"></v-text-field>
 
-        <v-text-field variant="outlined" v-model="password" label="Password"></v-text-field>
+        <v-text-field
+          variant="outlined"
+          v-model="password"
+          label="Password"
+          :type="passwordVisible ? 'text' : 'password'"
+          :append-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="togglePasswordVisibility"
+        ></v-text-field>
 
-        <v-text-field variant="outlined" v-model="confirmPassword" label="Confirm Password" type="password"></v-text-field>
+        <v-text-field
+          variant="outlined"
+          v-model="confirmPassword"
+          label="Confirm Password"
+          :type="confirmPasswordVisible ? 'text' : 'password'"
+          :append-icon="confirmPasswordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="toggleConfirmPasswordVisibility"
+        ></v-text-field>
 
-        <v-btn type="submit" color="primary" block class="mt-2">Register</v-btn>
+        <v-btn type="submit" color="primary" block class="mt-2">
+          Register
+        </v-btn>
       </v-form>
 
       <div class="mt-2">
-        <p class="text-body-2">Already have an account? <a href="/login">Sign In</a></p>
+        <p class="text-body-2">
+          Already have an account? <a href="/login">Sign In</a>
+        </p>
       </div>
     </v-sheet>
   </div>
@@ -28,7 +50,7 @@ export default {
   data() {
     return {
       username: '',
-      email:'',
+      email: '',
       password: '',
       confirm_password: '',
       passwordVisible: false,
@@ -42,16 +64,20 @@ export default {
         console.log('Email:', this.email);
         console.log('Password:', this.password);
 
-        const response = await axios.post('/register', {
-          username: this.username,
-          email: this.email,
-          password: this.password,
-          confirm_password: this.confirm_password,
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await axios.post(
+          '/register',
+          {
+            username: this.username,
+            email: this.email,
+            password: this.password,
+            confirm_password: this.confirm_password,
           },
-        });
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
         console.log('Response:', response.data);
       } catch (error) {
@@ -74,21 +100,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.headline {
-  text-align: center;
-  color: #2196F3; /* Vuetify primary color */
-}
-
-.link {
-  color: #2196F3; /* Vuetify primary color */
-  text-decoration: none;
-  cursor: pointer;
-}
-
-/* Add space below the buttons */
-.mt-4 {
-  margin-top: 1rem;
-}
-</style>
