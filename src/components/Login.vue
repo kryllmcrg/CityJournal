6ec6cb8c366c3170f66bd79185a06821142d3db0
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-14 12:42:17 +0800

    frontend

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,56 +1,20 @@
 <template>
-  <v-container class="container">
-    <v-row align="center">
-      <v-col cols="12">
-        <v-app-bar app color="#673ab7" dark>
-          <v-toolbar-title>
-            <v-img src="your-logo-url" alt="Logo" max-height="40"></v-img>
-          </v-toolbar-title>
-        </v-app-bar>
-      </v-col>
-    </v-row>
+  <div class="d-flex align-center justify-center" style="height: 100vh">
+      <v-sheet width="400" class="mx-auto">
+          <v-form fast-fail @submit.prevent="login">
+              <v-text-field variant="outlined" v-model="username" label="User Name"></v-text-field>
 
-    <!-- Main Content -->
-    <v-row justify="center">
-      <v-col cols="12" sm="10" md="8" lg="6">
-        <v-card elevation="12">
-          <v-card-title class="headline">
-            Login
-          </v-card-title>
+              <v-text-field variant="outlined" v-model="password" label="password"></v-text-field>
+              <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
 
-          <v-card-text>
-            <v-form ref="loginForm" @submit.prevent="login">
-              <v-text-field v-model="username" label="Username" outlined required></v-text-field>
-              <v-text-field
-                v-model="password"
-                label="Password"
-                :type="passwordVisible ? 'text' : 'password'"
-                :append-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="togglePasswordVisibility"
-                outlined
-                required
-              ></v-text-field>
-              <v-btn type="submit" color="#673ab7" class="mr-4">
-                Login
-              </v-btn>
-            </v-form>
+              <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
 
-            <v-row justify="center" class="mt-4">
-              <router-link to="/register" class="link">
-                Register
-              </router-link>
-
-              <span class="separator">|</span>
-
-              <a href="#" @click.prevent="forgotPassword" class="link">
-                Forgot Password
-              </a>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          </v-form>
+          <div class="mt-2">
+              <p class="text-body-2">Don't have an account? <a href="/register">Sign Up</a></p>
+          </div>
+      </v-sheet>
+  </div>
 </template>
 
 <script>
@@ -101,42 +65,16 @@ export default {
 <style scoped>
 .headline {
   text-align: center;
-  color: #673ab7; /* Vuetify primary color */
+  color: #2196F3; /* Vuetify primary color */
 }
 
 .link {
-  color: #673ab7; /* Vuetify primary color */
+  color: #2196F3; /* Vuetify primary color */
   text-decoration: none;
   cursor: pointer;
 }
 
 .separator {
   margin: 0 10px;
-}
-
-.container {
-  background-image: url('@/assets/bglogin.jpg');
-  background-size: cover;
-  background-position: center;
-<<<<<<< Updated upstream
-  height: 100vh; /* Set the height based on your design */
-  align-items: center;
-  justify-content: center;
-=======
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.v-card {
-  background-color: #ffffff; /* Card background color */
-  border-radius: 10px;
-  padding: 20px;
-}
-
-.v-btn {
-  border-radius: 5px;
->>>>>>> Stashed changes
 }
 </style>
