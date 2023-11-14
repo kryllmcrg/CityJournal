cbb50a254bd3fbe19918e4392cf7c318d06c658e
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-14 13:11:42 +0800

    frontend

--- src/components/register.vue
+++ src/components/register.vue
@@ -2,45 +2,24 @@
   <div class="d-flex align-center justify-center" style="height: 100vh">
     <v-sheet width="400" class="mx-auto">
       <v-form @submit.prevent="register">
-        <v-text-field
-          variant="outlined"
-          v-model="username"
-          label="Username"
-        ></v-text-field>
+        <v-text-field variant="outlined" v-model="username" label="Username"></v-text-field>
 
         <v-text-field variant="outlined" v-model="email" label="Email"></v-text-field>
 
-        <v-text-field
-          variant="outlined"
-          v-model="password"
-          label="Password"
-          :type="passwordVisible ? 'text' : 'password'"
-          :append-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="togglePasswordVisibility"
-        ></v-text-field>
+        <v-text-field variant="outlined" v-model="password" label="Password"></v-text-field>
 
-        <v-text-field
-          variant="outlined"
-          v-model="confirmPassword"
-          label="Confirm Password"
-          :type="confirmPasswordVisible ? 'text' : 'password'"
-          :append-icon="confirmPasswordVisible ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="toggleConfirmPasswordVisibility"
-        ></v-text-field>
+        <v-text-field variant="outlined" v-model="confirmPassword" label="Confirm Password" type="password"></v-text-field>
 
-        <v-btn type="submit" color="primary" block class="mt-2">
-          Register
-        </v-btn>
+        <v-btn type="submit" color="primary" block class="mt-2">Register</v-btn>
       </v-form>
 
       <div class="mt-2">
-        <p class="text-body-2">
-          Already have an account? <a href="/login">Sign In</a>
-        </p>
+        <p class="text-body-2">Already have an account? <a href="/login">Sign In</a></p>
       </div>
     </v-sheet>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
@@ -50,7 +29,7 @@ export default {
   data() {
     return {
       username: '',
-      email: '',
+      email:'',
       password: '',
       confirm_password: '',
       passwordVisible: false,
@@ -64,20 +43,16 @@ export default {
         console.log('Email:', this.email);
         console.log('Password:', this.password);
 
-        const response = await axios.post(
-          '/register',
-          {
-            username: this.username,
-            email: this.email,
-            password: this.password,
-            confirm_password: this.confirm_password,
+        const response = await axios.post('/register', {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          confirm_password: this.confirm_password,
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
           },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }
-        );
+        });
 
         console.log('Response:', response.data);
       } catch (error) {
