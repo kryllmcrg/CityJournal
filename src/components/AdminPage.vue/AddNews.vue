2970b3a4ab8257a374e3bf99853b031b0116f2b4
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-16 17:21:01 +0800

    Update AddNews.vue

--- src/components/AdminPage.vue/AddNews.vue
+++ src/components/AdminPage.vue/AddNews.vue
@@ -165,40 +165,35 @@
     </v-snackbar>
 
     <v-main>
-      <v-container>
-        <v-form @submit.prevent="submitNewsForm">
-          <v-card>
-            <v-card-title class="headline">Add News</v-card-title>
-            <v-card-text>
-              <!-- ... (your existing form fields) ... -->
+    <v-container>
+      <!-- Wrap the v-form around your form elements -->
+      <v-form @submit.prevent="submitNewsForm">
+        <v-card>
+          <v-card-title class="headline">Add News</v-card-title>
+          <v-card-text>
+            <!-- Title -->
+            <v-text-field v-model="newsTitle" label="Title" required></v-text-field>
 
-              <v-row>
-                <v-col cols="12">
-                  <!-- Include the Quill editor here -->
-                  <div class="quill-editor-container">
-                    <QuillEditor theme="snow" toolbar="#custom-toolbar" v-model="newsContent">
-                      <template #toolbar>
-                        <div id="custom-toolbar">
-                          <button class="ql-bold"></button>
-                          <button class="ql-italic"></button>
-                          <button class="ql-underline"></button>
-                          <button class="ql-strike"></button>
-                          <button id="your-button">Y</button>
-                        </div>
-                      </template>
-                    </QuillEditor>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn type="submit" color="primary">Save News</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-form>
-      </v-container>
-    </v-main>
+            <!-- Category -->
+            <v-select v-model="newsCategory" :items="categories" label="Category" required></v-select>
 
+            <!-- Author -->
+            <v-text-field v-model="newsAuthor" label="Author" required></v-text-field>
+
+            <!-- Stories of News -->
+            <v-textarea v-model="newsStories" label="Stories of News" required></v-textarea>
+
+            <!-- Image Upload -->
+            <v-file-input v-model="newsImage" label="Image" accept="image/*" required></v-file-input>
+          </v-card-text>
+          <v-card-actions>
+            <!-- Save News Button -->
+            <v-btn type="submit" color="primary" @submit.prevent="submitNewsForm">Save News</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-container>
+  </v-main>
 
     <!-- Message Box -->
     <v-snackbar v-model="showMessage" right>
@@ -218,9 +213,7 @@
 
 <script >
 import 'quill/dist/quill.snow.css';
-import VueQuillEditor from 'vue-quill-editor';
-
-const content = ref({})
+import VueQuillEditor from 'vue-quill-editor'
 
 export default {
   components: {
@@ -249,36 +242,23 @@ export default {
     selectItem(item) {
       this.selectedItem = item;
     },
+    submitNewsForm() {
+      // Handle the form submission logic here
+      console.log('Form submitted', {
+        title: this.newsTitle,
+        author: this.newsAuthor,
+        category: this.newsCategory,
+        image: this.newsImage,
+        stories: this.newsStories,
+        // ... (your existing data properties) ...
+    });
+  },
   },
 };
 </script>
 
   
   <style>
-.quill-editor-container {
-  width: 80%; /* Adjust the width as needed */
-  max-width: 800px; /* Set a maximum width for larger screens */
-  height: 300px; /* Adjust the height as needed */
-  margin: 50px auto; /* Center horizontally and add some top margin */
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow for depth */
-  border-radius: 8px; /* Add rounded corners for a softer look */
-  overflow: hidden; /* Hide overflowing content */
-}
-
-#custom-toolbar {
-  background-color: #f5f5f5; /* Set a light background color for the toolbar */
-  border-bottom: 1px solid #ddd; /* Add a bottom border for separation */
-  padding: 8px; /* Add padding for spacing within the toolbar */
-}
-
-/* Style Quill editor content area */
-.ql-editor {
-  font-family: 'Arial', sans-serif; /* Set a default font family */
-  font-size: 16px; /* Set a default font size */
-  color: #333; /* Set a default text color */
-  padding: 15px; /* Add padding within the editor */
-  line-height: 1.6; /* Set the line height for better readability */
-}
   .background-container {
     position:fixed;
     top: 0;
