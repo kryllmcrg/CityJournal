5d45faf33cebc554b150e328e3dcf79127179370
jhoannad <144778227+jhoannad@users.noreply.github.com>
2023-11-17 23:03:05 +0800

    Update Landing.vue

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,5 +1,14 @@
 <template>
     <v-card>
+      <!-- Design outside the navigation drawer -->
+      <v-container fluid class="outside-design">
+        <v-row justify="center" align="center">
+          <!-- Logo or any other content -->
+          <v-img src="../assets/logocalapan.png" alt="Logo"></v-img>
+        </v-row>
+      </v-container>
+  
+      <!-- Navigation Drawer -->
       <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false" class="custom-drawer">
         <!-- Menu Bar -->
         <v-app-bar app>
@@ -46,8 +55,14 @@
   <style>
   .custom-drawer {
     right: 0;
-    width: 4000px; /* Adjust the width as per your requirement */
+    width: 400px; /* Adjust the width as per your requirement */
   }
+  
+  .outside-design {
+  height: 200px; /* Set the height as per your requirement */
+  background: url('../assets/bglogin.jpg') center/cover; /* Replace 'bglogin.jpg' with your actual background image */
+  background-size: 100% 100%; /* Full coverage */
+}
   
   .v-list-item:hover {
     background-color: #9575cd;
