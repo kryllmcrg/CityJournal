4d250c84a1a8027ddabb8fded1bf011ba965fe7e
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-11-18 11:47:16 +0800

    Update User.vue

--- src/components/UserPage.vue/User.vue
+++ src/components/UserPage.vue/User.vue
@@ -1,42 +1,84 @@
 <template>
-    <v-container>
-      <!-- Page Title -->
-      <v-row justify="center">
-        <v-col cols="12" md="8">
-          <v-card class="elevation-12">
-            <v-card-title class="headline primary--text text--darken-1">
-              Approved News
-            </v-card-title>
-          </v-card>
-        </v-col>
-      </v-row>
-  
-     <!-- News Cards -->
-        <v-row justify="center">
-        <v-col v-for="newsItem in approvedNews" :key="newsItem.id" cols="12" md="4">
-            <v-card class="news-card">
-            <v-img :src="newsItem.image" height="200px"></v-img>
-            <v-card-title>{{ newsItem.title }}</v-card-title>
-            <v-card-subtitle>{{ newsItem.date }}</v-card-subtitle>
-            <v-card-subtitle>Author: {{ newsItem.author }}</v-card-subtitle>
-            <v-card-subtitle>Category: {{ newsItem.category }}</v-card-subtitle>
-            <v-card-text>{{ newsItem.summary }}</v-card-text>
-            <v-card-text>Stories: {{ newsItem.stories }}</v-card-text>
-            <v-card-text>Created At: {{ newsItem.created_at }}</v-card-text>
-            <v-card-text>Updated At: {{ newsItem.updated_at }}</v-card-text>
-            <v-card-actions>
-                <v-btn color="primary" @click="viewDetails(newsItem.id)">View Details</v-btn>
-            </v-card-actions>
-            </v-card>
-        </v-col>
-        </v-row>
-    </v-container>
-  </template>
+  <v-container>
+    <!-- Page Title -->
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card class="elevation-12">
+          <v-card-title class="headline primary--text text--darken-1">
+            Approved News
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- Category Filter Form -->
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-form @submit.prevent="filterNews">
+          <v-select
+            v-model="selectedCategory"
+            :items="categories"
+            label="Select Category"
+          ></v-select>
+          <v-btn type="submit" color="primary">Filter</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+
+    <!-- News Cards -->
+    <v-row justify="center">
+      <v-col v-for="newsItem in approvedNews" :key="newsItem.id" cols="12" md="4">
+        <v-card class="news-card">
+          <v-img :src="newsItem.image" height="200px"></v-img>
+          <v-card-title>{{ newsItem.title }}</v-card-title>
+          <v-card-subtitle>{{ newsItem.date }}</v-card-subtitle>
+          <v-card-subtitle>Author: {{ newsItem.author }}</v-card-subtitle>
+          <v-card-subtitle>Category: {{ newsItem.category }}</v-card-subtitle>
+          <v-card-text>{{ newsItem.summary }}</v-card-text>
+          <v-card-text>Stories: {{ newsItem.stories }}</v-card-text>
+          <v-card-text>Created At: {{ newsItem.created_at }}</v-card-text>
+          <v-card-text>Updated At: {{ newsItem.updated_at }}</v-card-text>
+
+          <!-- Comments Section -->
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="primary" @click="viewDetails(newsItem.id)">View Details</v-btn>
+          </v-card-actions>
+          <v-divider></v-divider>
+          <v-card-subtitle class="comment-header">Comments:</v-card-subtitle>
+          <v-list>
+            <!-- Iterate over comments for this news item -->
+            <v-list-item v-for="comment in newsItem.comments" :key="comment.id">
+              <v-list-item-content>{{ comment.text }}</v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-divider></v-divider>
+          <!-- Add a form for users to add new comments -->
+          <v-form @submit.prevent="addComment(newsItem)">
+            <v-textarea v-model="newsItem.newComment" label="Add a comment"></v-textarea>
+            <v-btn type="submit" color="primary">Post Comment</v-btn>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- Footer Section -->
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-footer app>
+          <!-- Your footer content goes here -->
+          <v-spacer></v-spacer>
+          <span>&copy; 2023 Your News App</span>
+        </v-footer>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
   
   <script>
   export default {
     data() {
       return {
+        electedCategory: null,
+        categories: ['General', 'Technology', 'Sports', 'Entertainment'],
         approvedNews: [
         {
             id: 1,
@@ -77,7 +119,44 @@
             updated_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
         },
 
-        
+        {
+            id: 1,
+            title: 'Breaking News 1',
+            author: 'John Doe',
+            category: 'General',
+            date: 'November 17, 2023',
+            summary: 'A brief summary of the breaking news.',
+            stories: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            image: 'https://preview.redd.it/kxov0rcoi4p21.png?auto=webp&s=8edd6e729acc84e01d55ad0853c7d5c07d86bada',
+            created_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+            updated_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+        },
+
+        {
+            id: 1,
+            title: 'Breaking News 1',
+            author: 'John Doe',
+            category: 'General',
+            date: 'November 17, 2023',
+            summary: 'A brief summary of the breaking news.',
+            stories: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            image: 'https://preview.redd.it/kxov0rcoi4p21.png?auto=webp&s=8edd6e729acc84e01d55ad0853c7d5c07d86bada',
+            created_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+            updated_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+        },
+
+        {
+            id: 1,
+            title: 'Breaking News 1',
+            author: 'John Doe',
+            category: 'General',
+            date: 'November 17, 2023',
+            summary: 'A brief summary of the breaking news.',
+            stories: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
+            image: 'https://preview.redd.it/kxov0rcoi4p21.png?auto=webp&s=8edd6e729acc84e01d55ad0853c7d5c07d86bada',
+            created_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+            updated_at: '2023-11-17T12:00:00', // Replace with the actual timestamp
+        },
         // Add more news items as needed
         ],
 
@@ -88,6 +167,12 @@
         // Implement logic to navigate to the details page for the selected news item
         // You can use Vue Router for navigation
         // this.$router.push(`/news/${newsId}`);
+        const newComment = {
+        id: newsItem.comments.length + 1,
+        text: newsItem.newComment,
+      };
+      newsItem.comments.push(newComment);
+      newsItem.newComment = ''; // Clear the input field after posting the comment
       },
     },
   };
@@ -102,5 +187,9 @@
   .news-card:hover {
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
   }
+  .comment-header {
+  font-weight: bold;
+  margin-top: 10px;
+}
   </style>
   
